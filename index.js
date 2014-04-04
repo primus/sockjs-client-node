8e9867f9f3ae8ed3d7057b86dc31638516834b57
3rd-Eden <info@3rd-Eden.com>
2014-04-04 22:27:32 +0200

    [fix] Depend on the primus compiled library

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var scope = require('jsdom').jsdom().createWindow()
-  , library = __dirname + '/library.js'
-  , read = require('fs').readFileSync;
+  , primus = require.resolve('primus')
+  , read = require('fs').readFileSync
+  , path = require('path');
+
+//
+// The library in Primus contains additional stability fixes which makes it
+// a lot more stable to run on node, which why we use that instead.
+//
+var library = path.join(primus, '..', 'transformers/sockjs/library.js');
 
 scope.WebSocket = require('ws');
 scope.eval(read(library, 'utf-8'));
