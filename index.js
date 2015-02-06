29f04b7dfb5fba941a645fd0817e5444efdfc9f5
Arnout Kazemier <info@3rd-Eden.com>
2015-02-06 10:57:56 +0100

    [major] Upgrade to latest JSDOM and WS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var scope = require('jsdom').jsdom().createWindow()
+var scope = require('jsdom').jsdom('<html><head></head><body></body></html>').parentWindow
   , primus = require.resolve('primus')
   , read = require('fs').readFileSync
   , path = require('path');
@@ -14,4 +14,10 @@ var library = path.join(primus, '..', 'transformers/sockjs/library.js');
 scope.WebSocket = require('ws');
 scope.eval(read(library, 'utf-8'));
 
+//
+// The href is `about:blank` by default. This doesn't work SockJS's same origin
+// check as it cannot parse it and it start throwing errors when it attempts to
+// find the host. So in order to fix this we need to force a HREF.
+//
+scope.location.href = 'http://localhost:8080/';
 module.exports = scope.SockJS;
